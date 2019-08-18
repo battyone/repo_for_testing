4e5dbed1cb619f84ab13831e2dadd5bcafc8667d
schigin <chigin.sergey@yandex.ru>
2019-08-18 15:34:05 +0300

    script updates + input data

--- simple_script.py
+++ simple_script.py
@@ -1,7 +1,18 @@
 import numpy as np
+import os
+import pandas as pd
+
+
 
 a=np.array([1,2,3,4,8])
 b=a*2-3
+
+os.chdir(r'C:\Users\Чигин Сергей\Desktop\repo_for_testing\input_data')
+df=pd.read_excel('data_sample.xls')
+
+c=df['column_1'].max()
+
+b=b+c
 
 print(b)
 
